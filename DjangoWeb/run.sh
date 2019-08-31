5d9c43082fbf4d7ceae150e021f572f2178d6993
hwj <478243824@qq.com>
2019-08-31 08:08:28 +0800

    docker-compose ports的规则是 宿主机端口:容器端口

--- DjangoWeb/run.sh
+++ DjangoWeb/run.sh
@@ -3,4 +3,5 @@
 python manage.py migrate
 
 # can't use 8080 port, because 8080 is used by jenkins
-python manage.py runserver 0.0.0.0:80
+# 上面的comment是错的，容器内根本不影响，要改的是docker-compose.yml
+python manage.py runserver 0.0.0.0:8080
